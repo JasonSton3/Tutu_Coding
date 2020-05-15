0874df4e40b70377df2185a62717867ecb8a78db
JasonSton3 <48706626+JasonSton3@users.noreply.github.com>
2020-05-15 14:27:50 +0800

    20200515_v1
    1. add version label at the bottom-right of the Form1.panel

--- tutuGUI_Jason_v3/tutuGUI_Jason_v3/Form1.Designer.vb
+++ tutuGUI_Jason_v3/tutuGUI_Jason_v3/Form1.Designer.vb
@@ -22,203 +22,215 @@ Partial Class Form1
     '請勿使用程式碼編輯器進行修改。
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
-        Me.Button10 = New System.Windows.Forms.Button()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
-        Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Button7 = New System.Windows.Forms.Button()
-        Me.Button8 = New System.Windows.Forms.Button()
-        Me.UserControl31 = New tutuGUI_Jason_v3.UserControl3()
-        Me.UserControl22 = New tutuGUI_Jason_v3.UserControl2()
-        Me.UserControl12 = New tutuGUI_Jason_v3.UserControl1()
-        Me.Panel3 = New System.Windows.Forms.Panel()
-        Me.Panel1.SuspendLayout()
-        Me.SuspendLayout()
-        '
-        'Button1
-        '
-        Me.Button1.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(156, 15)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(115, 35)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "EQ"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.Location = New System.Drawing.Point(287, 15)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(115, 35)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "DRC"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'Button3
-        '
-        Me.Button3.Location = New System.Drawing.Point(606, 23)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(55, 27)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Plot"
-        Me.Button3.UseVisualStyleBackColor = True
-        '
-        'Button5
-        '
-        Me.Button5.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button5.Location = New System.Drawing.Point(826, 15)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(115, 35)
-        Me.Button5.TabIndex = 7
-        Me.Button5.Text = "Copy"
-        Me.Button5.UseVisualStyleBackColor = True
-        '
-        'Button10
-        '
-        Me.Button10.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button10.Location = New System.Drawing.Point(704, 15)
-        Me.Button10.Name = "Button10"
-        Me.Button10.Size = New System.Drawing.Size(115, 35)
-        Me.Button10.TabIndex = 15
-        Me.Button10.Text = "Paste"
-        Me.Button10.UseVisualStyleBackColor = True
-        '
-        'Panel1
-        '
-        Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Location = New System.Drawing.Point(457, 3)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(209, 57)
-        Me.Panel1.TabIndex = 19
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(25, 22)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(157, 12)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Plot and Dev-Panel is under here"
-        '
-        'CheckBox1
-        '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox1.Location = New System.Drawing.Point(26, 16)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(81, 23)
-        Me.CheckBox1.TabIndex = 20
-        Me.CheckBox1.Text = "Tutu ON"
-        Me.CheckBox1.UseVisualStyleBackColor = True
-        '
-        'CheckBox2
-        '
-        Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox2.Location = New System.Drawing.Point(26, 41)
-        Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(85, 23)
-        Me.CheckBox2.TabIndex = 21
-        Me.CheckBox2.Text = "Tutu OFF"
-        Me.CheckBox2.UseVisualStyleBackColor = True
-        '
-        'Panel2
-        '
-        Me.Panel2.Location = New System.Drawing.Point(1, 62)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(1432, 698)
-        Me.Panel2.TabIndex = 22
-        '
-        'Button7
-        '
-        Me.Button7.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button7.Location = New System.Drawing.Point(986, 17)
-        Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(62, 28)
-        Me.Button7.TabIndex = 24
-        Me.Button7.Text = "debug"
-        Me.Button7.UseVisualStyleBackColor = True
-        '
-        'Button8
-        '
-        Me.Button8.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button8.Location = New System.Drawing.Point(463, 17)
-        Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(108, 34)
-        Me.Button8.TabIndex = 25
-        Me.Button8.Text = "Developer"
-        Me.Button8.UseVisualStyleBackColor = True
-        '
-        'UserControl31
-        '
-        Me.UserControl31.Location = New System.Drawing.Point(52, 91)
-        Me.UserControl31.Name = "UserControl31"
-        Me.UserControl31.Size = New System.Drawing.Size(1282, 583)
-        Me.UserControl31.TabIndex = 0
-        '
-        'UserControl22
-        '
-        Me.UserControl22.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.UserControl22.Location = New System.Drawing.Point(37, 62)
-        Me.UserControl22.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
-        Me.UserControl22.Name = "UserControl22"
-        Me.UserControl22.Size = New System.Drawing.Size(1332, 675)
-        Me.UserControl22.TabIndex = 17
-        '
-        'UserControl12
-        '
-        Me.UserControl12.BackColor = System.Drawing.SystemColors.Control
-        Me.UserControl12.Location = New System.Drawing.Point(37, 62)
-        Me.UserControl12.Name = "UserControl12"
-        Me.UserControl12.Size = New System.Drawing.Size(1385, 683)
-        Me.UserControl12.TabIndex = 16
-        '
-        'Panel3
-        '
-        Me.Panel3.Location = New System.Drawing.Point(972, 17)
-        Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(112, 30)
-        Me.Panel3.TabIndex = 26
-        '
-        'Form1
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1434, 761)
-        Me.Controls.Add(Me.Panel3)
-        Me.Controls.Add(Me.Panel1)
-        Me.Controls.Add(Me.Button8)
-        Me.Controls.Add(Me.UserControl31)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button7)
-        Me.Controls.Add(Me.Panel2)
-        Me.Controls.Add(Me.CheckBox2)
-        Me.Controls.Add(Me.CheckBox1)
-        Me.Controls.Add(Me.UserControl22)
-        Me.Controls.Add(Me.UserControl12)
-        Me.Controls.Add(Me.Button10)
-        Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.Name = "Form1"
-        Me.Text = "Tutu Parameter Control Panel"
-        Me.Panel1.ResumeLayout(False)
-        Me.Panel1.PerformLayout()
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.Button2 = New System.Windows.Forms.Button()
+		Me.Button3 = New System.Windows.Forms.Button()
+		Me.Button5 = New System.Windows.Forms.Button()
+		Me.Button10 = New System.Windows.Forms.Button()
+		Me.Panel1 = New System.Windows.Forms.Panel()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+		Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+		Me.Panel2 = New System.Windows.Forms.Panel()
+		Me.Button7 = New System.Windows.Forms.Button()
+		Me.Button8 = New System.Windows.Forms.Button()
+		Me.UserControl31 = New tutuGUI_Jason_v3.UserControl3()
+		Me.UserControl22 = New tutuGUI_Jason_v3.UserControl2()
+		Me.UserControl12 = New tutuGUI_Jason_v3.UserControl1()
+		Me.Panel3 = New System.Windows.Forms.Panel()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.Panel1.SuspendLayout()
+		Me.SuspendLayout()
+		'
+		'Button1
+		'
+		Me.Button1.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button1.Location = New System.Drawing.Point(156, 15)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(115, 35)
+		Me.Button1.TabIndex = 0
+		Me.Button1.Text = "EQ"
+		Me.Button1.UseVisualStyleBackColor = True
+		'
+		'Button2
+		'
+		Me.Button2.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button2.Location = New System.Drawing.Point(287, 15)
+		Me.Button2.Name = "Button2"
+		Me.Button2.Size = New System.Drawing.Size(115, 35)
+		Me.Button2.TabIndex = 1
+		Me.Button2.Text = "DRC"
+		Me.Button2.UseVisualStyleBackColor = True
+		'
+		'Button3
+		'
+		Me.Button3.Location = New System.Drawing.Point(606, 23)
+		Me.Button3.Name = "Button3"
+		Me.Button3.Size = New System.Drawing.Size(55, 27)
+		Me.Button3.TabIndex = 2
+		Me.Button3.Text = "Plot"
+		Me.Button3.UseVisualStyleBackColor = True
+		'
+		'Button5
+		'
+		Me.Button5.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button5.Location = New System.Drawing.Point(826, 15)
+		Me.Button5.Name = "Button5"
+		Me.Button5.Size = New System.Drawing.Size(115, 35)
+		Me.Button5.TabIndex = 7
+		Me.Button5.Text = "Copy"
+		Me.Button5.UseVisualStyleBackColor = True
+		'
+		'Button10
+		'
+		Me.Button10.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button10.Location = New System.Drawing.Point(704, 15)
+		Me.Button10.Name = "Button10"
+		Me.Button10.Size = New System.Drawing.Size(115, 35)
+		Me.Button10.TabIndex = 15
+		Me.Button10.Text = "Paste"
+		Me.Button10.UseVisualStyleBackColor = True
+		'
+		'Panel1
+		'
+		Me.Panel1.Controls.Add(Me.Label1)
+		Me.Panel1.Location = New System.Drawing.Point(457, 3)
+		Me.Panel1.Name = "Panel1"
+		Me.Panel1.Size = New System.Drawing.Size(209, 57)
+		Me.Panel1.TabIndex = 19
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(25, 22)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(157, 12)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "Plot and Dev-Panel is under here"
+		'
+		'CheckBox1
+		'
+		Me.CheckBox1.AutoSize = True
+		Me.CheckBox1.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.CheckBox1.Location = New System.Drawing.Point(26, 16)
+		Me.CheckBox1.Name = "CheckBox1"
+		Me.CheckBox1.Size = New System.Drawing.Size(81, 23)
+		Me.CheckBox1.TabIndex = 20
+		Me.CheckBox1.Text = "Tutu ON"
+		Me.CheckBox1.UseVisualStyleBackColor = True
+		'
+		'CheckBox2
+		'
+		Me.CheckBox2.AutoSize = True
+		Me.CheckBox2.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.CheckBox2.Location = New System.Drawing.Point(26, 41)
+		Me.CheckBox2.Name = "CheckBox2"
+		Me.CheckBox2.Size = New System.Drawing.Size(85, 23)
+		Me.CheckBox2.TabIndex = 21
+		Me.CheckBox2.Text = "Tutu OFF"
+		Me.CheckBox2.UseVisualStyleBackColor = True
+		'
+		'Panel2
+		'
+		Me.Panel2.Location = New System.Drawing.Point(1, 62)
+		Me.Panel2.Name = "Panel2"
+		Me.Panel2.Size = New System.Drawing.Size(1432, 698)
+		Me.Panel2.TabIndex = 22
+		'
+		'Button7
+		'
+		Me.Button7.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button7.Location = New System.Drawing.Point(986, 17)
+		Me.Button7.Name = "Button7"
+		Me.Button7.Size = New System.Drawing.Size(62, 28)
+		Me.Button7.TabIndex = 24
+		Me.Button7.Text = "debug"
+		Me.Button7.UseVisualStyleBackColor = True
+		'
+		'Button8
+		'
+		Me.Button8.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button8.Location = New System.Drawing.Point(463, 17)
+		Me.Button8.Name = "Button8"
+		Me.Button8.Size = New System.Drawing.Size(108, 34)
+		Me.Button8.TabIndex = 25
+		Me.Button8.Text = "Developer"
+		Me.Button8.UseVisualStyleBackColor = True
+		'
+		'UserControl31
+		'
+		Me.UserControl31.Location = New System.Drawing.Point(52, 91)
+		Me.UserControl31.Name = "UserControl31"
+		Me.UserControl31.Size = New System.Drawing.Size(1282, 583)
+		Me.UserControl31.TabIndex = 0
+		'
+		'UserControl22
+		'
+		Me.UserControl22.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.UserControl22.Location = New System.Drawing.Point(37, 62)
+		Me.UserControl22.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+		Me.UserControl22.Name = "UserControl22"
+		Me.UserControl22.Size = New System.Drawing.Size(1332, 675)
+		Me.UserControl22.TabIndex = 17
+		'
+		'UserControl12
+		'
+		Me.UserControl12.BackColor = System.Drawing.SystemColors.Control
+		Me.UserControl12.Location = New System.Drawing.Point(37, 62)
+		Me.UserControl12.Name = "UserControl12"
+		Me.UserControl12.Size = New System.Drawing.Size(1385, 683)
+		Me.UserControl12.TabIndex = 16
+		'
+		'Panel3
+		'
+		Me.Panel3.Location = New System.Drawing.Point(972, 17)
+		Me.Panel3.Name = "Panel3"
+		Me.Panel3.Size = New System.Drawing.Size(112, 30)
+		Me.Panel3.TabIndex = 26
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label2.Location = New System.Drawing.Point(1233, 762)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(197, 15)
+		Me.Label2.TabIndex = 27
+		Me.Label2.Text = "Tutu Spectimbre Version Aplha_1.00"
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(1432, 780)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.Panel3)
+		Me.Controls.Add(Me.Panel1)
+		Me.Controls.Add(Me.Button8)
+		Me.Controls.Add(Me.UserControl31)
+		Me.Controls.Add(Me.Button3)
+		Me.Controls.Add(Me.Button7)
+		Me.Controls.Add(Me.Panel2)
+		Me.Controls.Add(Me.CheckBox2)
+		Me.Controls.Add(Me.CheckBox1)
+		Me.Controls.Add(Me.UserControl22)
+		Me.Controls.Add(Me.UserControl12)
+		Me.Controls.Add(Me.Button10)
+		Me.Controls.Add(Me.Button5)
+		Me.Controls.Add(Me.Button2)
+		Me.Controls.Add(Me.Button1)
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+		Me.Name = "Form1"
+		Me.Text = "Tutu Parameter Control Panel"
+		Me.Panel1.ResumeLayout(False)
+		Me.Panel1.PerformLayout()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
 
-    End Sub
+	End Sub
 
-    Friend WithEvents Button1 As Button
+	Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
     Friend WithEvents UserControl11 As UserControl1
@@ -236,4 +248,5 @@ Partial Class Form1
     Friend WithEvents Button8 As Button
     Friend WithEvents UserControl31 As UserControl3
     Friend WithEvents Panel3 As Panel
+    Friend WithEvents Label2 As Label
 End Class
